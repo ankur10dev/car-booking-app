0e7d3e054a416679ebcb0f23fd94aa9cd70b49b7
ankur <ankurjayasingh@gmail.com>
2024-07-03 07:06:00 +0530

    first commit

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { fadeIn, staggerContainer } from "@/contants/motion";
-import styles from "@/contants/styles";
+import { fadeIn, staggerContainer } from "@/contsants/motion";
+import styles from "@/contsants/styles";
 
 const Feedback = () => {
   return (
